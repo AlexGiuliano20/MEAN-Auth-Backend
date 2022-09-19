83a1fe12211a7484401ecebbf4c75614f58b1aaf
AlexGiuliano20 <alex.giuliano37@gmail.com>
2022-09-18 23:07:32 -0300

    Validar campos obligatorios login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const { response } = require("express");
+const { validationResult } = require("express-validator");
 
 const crearUsuario = (req, res = response) => {
 	const { email, name, password } = req.body;
@@ -11,8 +12,17 @@ const crearUsuario = (req, res = response) => {
 };
 
 const loginUsuario = (req, res) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json({
+			ok: false,
+			errors: errors.mapped(),
+		});
+	}
+
 	const { email, password } = req.body;
 	console.log(email, password);
+
 	return res.json({
 		ok: true,
 		msg: "Login de usuario",
